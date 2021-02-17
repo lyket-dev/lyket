7c5e1bb19eea1c11da47c1ce77cc48053cb9f8eb
Irene Oppo <i.oppo@datocms.com>
2021-02-17 15:33:10 +0100

    Execute widget script only on DOMContentLoaded

--- packages/widget/src/index.js
+++ packages/widget/src/index.js
@@ -1,100 +1,102 @@
 import { h, render } from 'preact';
 import { Provider, ClapButton, LikeButton, UpdownButton } from '@lyket/react';
 
-function getUrlParameter(url, name) {
-  const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-  const results = regex.exec(url);
-
-  return results === null
-    ? ''
-    : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
 const scriptSrc = document.currentScript.getAttribute('src');
 
-const baseUrl = getUrlParameter(scriptSrc, 'baseUrl');
-const recaptchaSiteKey = getUrlParameter(scriptSrc, 'recaptchaSiteKey');
-const apiKey = getUrlParameter(scriptSrc, 'apiKey');
+document.addEventListener('DOMContentLoaded', function(_event) {
+  function getUrlParameter(url, name) {
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    const results = regex.exec(url);
 
-if (!apiKey) {
-  console.error('Lyket Error: Api key missing!');
-} else {
-  const templateHashGenerator = button => {
-    const templateObj = {};
+    return results === null
+      ? ''
+      : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
 
-    Object.keys(button.templates).forEach(
-      key => (templateObj[key.toLowerCase()] = button.templates[key])
-    );
+  const baseUrl = getUrlParameter(scriptSrc, 'baseUrl');
+  const recaptchaSiteKey = getUrlParameter(scriptSrc, 'recaptchaSiteKey');
+  const apiKey = getUrlParameter(scriptSrc, 'apiKey');
 
-    return templateObj;
-  };
+  if (!apiKey) {
+    console.error('Lyket Error: Api key missing!');
+  } else {
+    const templateHashGenerator = button => {
+      const templateObj = {};
 
-  const typeToButton = {
-    clap: {
-      component: ClapButton,
-      templates: templateHashGenerator(ClapButton),
-    },
-    updown: {
-      component: UpdownButton,
-      templates: templateHashGenerator(UpdownButton),
-    },
-    like: {
-      component: LikeButton,
-      templates: templateHashGenerator(LikeButton),
-    },
-  };
+      Object.keys(button.templates).forEach(
+        key => (templateObj[key.toLowerCase()] = button.templates[key])
+      );
 
-  document.querySelectorAll('[data-lyket-type]').forEach(element => {
-    const {
-      lyketId: id,
-      lyketNamespace: namespace,
-      lyketType: type,
-      lyketHideCounter: hideCounterIfLessThan,
-      lyketTemplate: template,
-      lyketColorBackground,
-      lyketColorPrimary,
-      lyketColorSecondary,
-      lyketColorText,
-      lyketColorHighlight,
-    } = element.dataset;
+      return templateObj;
+    };
 
-    const providerProps = { baseUrl, recaptchaSiteKey, apiKey };
+    const typeToButton = {
+      clap: {
+        component: ClapButton,
+        templates: templateHashGenerator(ClapButton),
+      },
+      updown: {
+        component: UpdownButton,
+        templates: templateHashGenerator(UpdownButton),
+      },
+      like: {
+        component: LikeButton,
+        templates: templateHashGenerator(LikeButton),
+      },
+    };
 
-    if (
-      lyketColorBackground ||
-      lyketColorPrimary ||
-      lyketColorSecondary ||
-      lyketColorText ||
-      lyketColorHighlight
-    ) {
-      providerProps.theme = { colors: {} };
+    document.querySelectorAll('[data-lyket-type]').forEach(element => {
+      const {
+        lyketId: id,
+        lyketNamespace: namespace,
+        lyketType: type,
+        lyketHideCounter: hideCounterIfLessThan,
+        lyketTemplate: template,
+        lyketColorBackground,
+        lyketColorPrimary,
+        lyketColorSecondary,
+        lyketColorText,
+        lyketColorHighlight,
+      } = element.dataset;
 
-      if (lyketColorBackground)
-        providerProps.theme.colors.background = lyketColorBackground;
-      if (lyketColorPrimary)
-        providerProps.theme.colors.primary = lyketColorPrimary;
-      if (lyketColorSecondary)
-        providerProps.theme.colors.secondary = lyketColorSecondary;
-      if (lyketColorText) providerProps.theme.colors.text = lyketColorText;
-      if (lyketColorHighlight)
-        providerProps.theme.colors.highlight = lyketColorHighlight;
-    }
+      const providerProps = { baseUrl, recaptchaSiteKey, apiKey };
 
-    const button = typeToButton[type.toLowerCase()];
+      if (
+        lyketColorBackground ||
+        lyketColorPrimary ||
+        lyketColorSecondary ||
+        lyketColorText ||
+        lyketColorHighlight
+      ) {
+        providerProps.theme = { colors: {} };
 
-    if (!id) {
-      console.error('Lyket Error: Missing ID!');
-    } else if (!button) {
-      console.error(`Lyket Error: "${type}" is not a valid button type`);
-    } else {
-      const buttonProps = { id, namespace, hideCounterIfLessThan };
-
-      if (template) {
-        buttonProps.component = button.templates[template.toLowerCase()];
+        if (lyketColorBackground)
+          providerProps.theme.colors.background = lyketColorBackground;
+        if (lyketColorPrimary)
+          providerProps.theme.colors.primary = lyketColorPrimary;
+        if (lyketColorSecondary)
+          providerProps.theme.colors.secondary = lyketColorSecondary;
+        if (lyketColorText) providerProps.theme.colors.text = lyketColorText;
+        if (lyketColorHighlight)
+          providerProps.theme.colors.highlight = lyketColorHighlight;
       }
 
-      const Component = button.component;
-      render(h(Provider, providerProps, h(Component, buttonProps)), element);
-    }
-  });
-}
+      const button = typeToButton[type.toLowerCase()];
+
+      if (!id) {
+        console.error('Lyket Error: Missing ID!');
+      } else if (!button) {
+        console.error(`Lyket Error: "${type}" is not a valid button type`);
+      } else {
+        const buttonProps = { id, namespace, hideCounterIfLessThan };
+
+        if (template) {
+          buttonProps.component = button.templates[template.toLowerCase()];
+        }
+
+        const Component = button.component;
+        render(h(Provider, providerProps, h(Component, buttonProps)), element);
+      }
+    });
+  }
+});
