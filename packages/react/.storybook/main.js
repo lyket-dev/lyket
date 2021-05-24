186839c81d174c060b63304c9b80fc25282ccf99
Irene Oppo <i.oppo@datocms.com>
2021-05-24 09:00:21 +0200

    Updated storybook

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,7 +1,11 @@
 module.exports = {
   stories: ['../stories/**/*.stories.(ts|tsx|mdx)'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-docs'],
-  webpackFinal: async (config) => {
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+  ],
+  webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
